888ccb79b6b3483804fd63b6920d8e15e4fb929d
cmacana <carlos.macana@zepben.com>
2021-03-23 09:41:22 +1100

    Add examples directory & transformer_examples.py
    Signed-off-by: cmacana <carlos.macana@zepben.com>

---
+++ utils/examples/transformer_examples.py
@@ -0,0 +1,5 @@
+from utils.transformer import *
+
+z = short_circuit_impedance_by_voltage(vk_percent=6, vkr_percent=1.1425, net_sn=400e3, sn=400e3)
+print(impedance_pu(z, sn_ref=400e3, vn_ref=400))
+
