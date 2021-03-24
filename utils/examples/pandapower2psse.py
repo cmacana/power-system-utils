ce1a14e2a99f5d4d2702ea4c033068493e82c0ea
cmacana <carlos.macana@zepben.com>
2021-03-24 21:26:16 +1100

    Add mappers for buses, lines and loads.
    Signed-off-by: cmacana <carlos.macana@zepben.com>

--- utils/examples/pandapower2psse.py
+++ utils/examples/pandapower2psse.py
@@ -1,18 +1,57 @@
-import grg_pssedata.struct as grg
+from grg_pssedata.struct import Case, Bus, Branch, Load
 
 from minimal_example import *
+from utils.calculations.pu import impedance_pu
 
-net = ThreeBusesFeederPP()
-print(net.get_load_flow_results())
-pp_network: pp.pandapowerNet = net.pp_network
-# print(list(pp_network.bus.iterrows()))
-print(pp_network.bus)
 
-buses = []
-loads = []
+def map_buses(pp_net: pp.pandapowerNet):
+    new_buses = []
+    print(net.bus)
+    for i, row in list(pp_net.bus.iterrows()):
+        if i == 0:
+            bus_type = 2
+        else:
+            bus_type = 1
+        args = [row['name'], row.vn_kv, bus_type, 1, 1, 1, 1.0, 0.0, 1.1, 0.9, 1.1, 0.9]
+        psse_bus = Bus(i + 1, *args)
+        new_buses.append(psse_bus)
+    return new_buses
+
+
+def map_loads(pp_net: pp.pandapowerNet):
+    new_loads = []
+    print(net.load)
+    for i, row in list(pp_net.load.iterrows()):
+        args = [row.bus + 1, i + 1, 1, 1, 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0]
+        psse_element = Load(i + 1, *args)
+        new_loads.append(psse_element)
+    return new_loads
+
+
+def map_lines(pp_net: pp.pandapowerNet):
+    new_lines = []
+    print(pp_net.line)
+    for i, row in list(pp_net.line.iterrows()):
+        psse_element = Branch(i + 1, i=row.from_bus + 1, j=row.to_bus + 1, ckt='',
+                              # TODO: Complete computation of  r & x
+                              r=impedance_pu(row.length_km * row.r_ohm_per_km, 1, 1),
+                              x=impedance_pu(row.length_km * row.x_ohm_per_km, 1, 1),
+                              b=0,
+                              ratea=0, rateb=0, ratec=0,
+                              gi=0, bi=0, gj=0, bj=0,
+                              st=1, met=1,
+                              len=row.length_km,
+                              o1=1, f1=1.0, o2=1, f2=1.0, o3=1, f3=1.0, o4=1, f4=1.0)
+        new_lines.append(psse_element)
+    return new_lines
+
+
+net = ThreeBusesFeederPP().pp_network
+buses = map_buses(net)
+loads = map_loads(net)
 fixed_shunts = []
 generators = []
-branches = []
+branches = map_lines(net)
 transformers = []
 areas = []
 tt_dc_lines = []
@@ -28,21 +67,14 @@ switched_shunts = []
 gnes = []
 induction_machines = []
 
-for i, row in list(pp_network.bus.iterrows()):
-    if i == 0:
-        bus_type = 2
-    else:
-        bus_type = 1
-    args = [row['name'], row.vn_kv, bus_type, 1, 1, 1, 1.0, 0.0, 1.1, 0.9, 1.1, 0.9]
-    psse_bus = grg.Bus(i+1, *args)
-    buses.append(psse_bus)
-
 ic, sbase, rev, xfrrat, nxfrat, basefrq = [0, 100.0, 33, 0, 0, 50]
-record1, record2 = ['PandaPower Mimimal Example', ""]
-case_defaults = [[] for _ in range(18)]
+record1, record2 = ['PandaPower Minimal Example', ""]
 
-case = grg.Case(ic, sbase, rev, xfrrat, nxfrat, basefrq, record1, record2,
-                buses, *case_defaults)
+case = Case(ic, sbase, rev, xfrrat, nxfrat, basefrq, record1, record2,
+            buses, loads, fixed_shunts, generators, branches, transformers, areas,
+            tt_dc_lines, vsc_dc_lines, transformer_corrections, mt_dc_lines,
+            line_groupings, zones, transfers, owners, facts, switched_shunts,
+            gnes, induction_machines)
 
 print(case.to_psse())
 print(case.validate())
